64d4ad29eb327b661df54c4f80fe3fdb59bcc438
vitordiniz25 <vitordiniz25@gmail.com>
2021-10-08 18:50:05 -0300

    Finalizando primeira aula

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,28 @@
-import type { NextPage } from 'next'
-import SafeEnviroment from 'ui/components/feedback/SafeEnviroment/SafeEnviroment'
-import { PageTitle } from 'ui/components/data-display/PageTitle/PageTitle'
+import type { NextPage } from "next";
+import SafeEnviroment from "ui/components/feedback/SafeEnviroment/SafeEnviroment";
+import { PageTitle } from "ui/components/data-display/PageTitle/PageTitle";
+import { UserInformation } from "ui/components/data-display/UserInformation/UserInformation";
+import { useReducer } from "react";
 
 const Home: NextPage = () => {
   return (
     <div>
       <SafeEnviroment />
       <PageTitle
-        title={'Conheça os profissionais'}
-        subtitle={'Preencha seu endereço e veja todos os profissionais da sua localidade'}
+        title={"Conheça os profissionais"}
+        subtitle={
+          "Preencha seu endereço e veja todos os profissionais da sua localidade"
+        }
+      />
+
+      <UserInformation
+        name={"Vitor Diniz"}
+        picture={"https://github.com/vitordiniz25.png"}
+        rating={3}
+        description={"Brasília"}
       />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- src/ui/components/surfaces/Header/Header.tsx
+++ src/ui/components/surfaces/Header/Header.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { HeaderAppBar, HeaderLogo } from "./Header.style";
-import { Toolbar } from "@mui/material";
+import { Toolbar, Container } from "@mui/material";
 
 export const Header = () => {
   return (
     <HeaderAppBar position={"sticky"}>
-      <Toolbar>
+      <Toolbar component={Container}>
         <HeaderLogo src="/img/logos/logo.svg" alt="e-diaristas" />
       </Toolbar>
     </HeaderAppBar>
