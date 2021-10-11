e2d05b461969dc826fe890f31cf0efe3b953eba3
vitordiniz25 <vitordiniz25@gmail.com>
2021-10-11 17:42:34 -0300

    Aula 2 parte 1

---
+++ src/ui/components/surfaces/Footer/Footer.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import {
+  FooterStyled,
+  FooterContainer,
+  FooterTitle,
+  AppList,
+} from "./Fotter.style";
+import { Typography, Box } from "@mui/material";
+
+export const Footer = () => {
+  return (
+    <FooterStyled>
+      <FooterContainer>
+        <Box sx={{ maxWidth: "400px" }}>
+          <FooterTitle>Quem Somos</FooterTitle>
+          <Typography variant={"body2"} sx={{ mt: 2 }}>
+            O e-diaristas te ajuda a encontrar um profissional perfeito para
+            realizar a limpeza da sua casa. Garantimos os melhores profissionais
+            com total segurança e praticidade! São milhares de clientes
+            satisfeitos por todo o país.
+          </Typography>
+        </Box>
+
+        <div>
+          <FooterTitle>Baixe nossos aplicativos</FooterTitle>
+          <AppList>
+            <li>
+              <a href={"/"} target={"_blank"} rel={"noopener noreferrer"}>
+                <img src={"/img/logos/app-store.png"} alt={"App Store"} />
+              </a>
+            </li>
+            <li>
+              <a href={"/"} target={"_blank"} rel={"noopener noreferrer"}>
+                <img src={"/img/logos/google-play.png"} alt={"Google Play"} />
+              </a>
+            </li>
+          </AppList>
+        </div>
+      </FooterContainer>
+    </FooterStyled>
+  );
+};
